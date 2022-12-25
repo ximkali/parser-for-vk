bc9a11ecaeb35f4ddb14d0bc3a3491170bea7540
GlaziroslikMilok <118281517+PlatzEins1@users.noreply.github.com>
2022-12-25 15:05:14 +0400

    Update urls.py

--- parcer_vk/python_app/urls.py
+++ parcer_vk/python_app/urls.py
@@ -3,3 +3,4 @@ url_get_by_id = 'https://api.vk.com/method/groups.getById?'
 url_get_members = 'https://api.vk.com/method/groups.getMembers?' Возвращает список участников сообзества
 url_wall_get = 'https://api.vk.com/method/wall.get?' Возвращает список записей со стены сообщества
 url_get_comments = 'https://api.vk.com/method/wall.getComments?' Возвращает комментарии со стены сообщества
+url_get_posts = 'https://api.vk.com/method/wall.get?' Возвращает список постов со стены
